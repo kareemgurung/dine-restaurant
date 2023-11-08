baf4668864c111ffb6fab566daf153e6bbbe3a24
Kareem Gurung <66091354+kareemgurung@users.noreply.github.com>
2023-11-08 15:03:46 +0000

    initial setup

--- tailwind.config.ts
+++ tailwind.config.ts
@@ -1,20 +1,30 @@
-import type { Config } from 'tailwindcss'
+import type { Config } from "tailwindcss";
 
 const config: Config = {
   content: [
-    './src/pages/**/*.{js,ts,jsx,tsx,mdx}',
-    './src/components/**/*.{js,ts,jsx,tsx,mdx}',
-    './src/app/**/*.{js,ts,jsx,tsx,mdx}',
+    "./src/pages/**/*.{js,ts,jsx,tsx,mdx}",
+    "./src/components/**/*.{js,ts,jsx,tsx,mdx}",
+    "./src/app/**/*.{js,ts,jsx,tsx,mdx}",
   ],
   theme: {
     extend: {
       backgroundImage: {
-        'gradient-radial': 'radial-gradient(var(--tw-gradient-stops))',
-        'gradient-conic':
-          'conic-gradient(from 180deg at 50% 50%, var(--tw-gradient-stops))',
+        "gradient-radial": "radial-gradient(var(--tw-gradient-stops))",
+        "gradient-conic":
+          "conic-gradient(from 180deg at 50% 50%, var(--tw-gradient-stops))",
+      },
+      colors: {
+        beaver: "#9E7F66",
+        "cod-gray": "#111111",
+        mirage: "#17192B",
+        "ebony-clay": "#242B37",
+        "shuttle-gray": "#5C6779",
+      },
+      screens: {
+        lg: "1440px",
       },
     },
   },
   plugins: [],
-}
-export default config
+};
+export default config;
