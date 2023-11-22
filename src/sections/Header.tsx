8a097c2fe91aa3d0c715069a71adc0c435d1ce1e
Kareem Gurung <66091354+kareemgurung@users.noreply.github.com>
2023-11-22 14:41:32 +0000

    Header section completed

--- src/sections/Header.tsx
+++ src/sections/Header.tsx
@@ -1,5 +1,23 @@
+import Image from "next/image";
+import Logo from "/public/images/logo.svg";
+import Button from "@/components/Button";
+
 const Header = () => {
-  return <section>Header</section>;
+  return (
+    <section className="bg-mirage header-bg">
+      <div className="px-6 pb-[152px] pt-[232px] flex flex-col items-center text-center md:px-10 md:pt-[374px] md:pb-[216px] lg:pt-16 lg:pb-[198px] lg:items-start lg:text-left lg:max-width ">
+        <Logo />
+        <div className="flex flex-col gap-5 mt-9 mb-14 md:px-14 lg:px-0 lg:mt-[153px] lg:mb-10 lg:gap-[10px] lg:w-[507px]">
+          <h1 className="text-white px-6 md:px-20 lg:px-0">Exquisite dining since 1989</h1>
+          <p className="text-white font-normal text-base/[1.625rem] md:text-[1.25rem]/[1.875rem] lg:pr-20">
+            Experience our seasonal menu in beautiful country surroundings. Eat
+            the freshest produce from the comfort of our farmhouse.
+          </p>
+        </div>
+        <Button title="book a table" variant="btn-dark" link="/booking" width="btn-small"/>
+      </div>
+    </section>
+  );
 };
 
 export default Header;
